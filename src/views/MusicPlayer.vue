63846f27228c2b40eb157367e5f7a7d5fb3e12a8
CENCHAOCHAO <cenchaochao@outlook.com>
2019-01-18 14:49:48 +0800

    feat: finish media-api

--- src/views/MusicPlayer.vue
+++ src/views/MusicPlayer.vue
@@ -9,8 +9,11 @@
     </div>
     <div class="progress-bar">
     </div>
+    <div class="player">
+      <audio ref="audio" src="http://113.215.22.149/amobile.music.tc.qq.com/C400000H2jfM21Wl7c.m4a?guid=1586627704&vkey=9BD098C0A3FD8073AB950EDC617218FAC160894E3ED54E2C939CD72719130E15E45846D723D2ACE1995568AFFDCBEAF78E4CA6DCD4CF5520&uin=0&fromtag=66" controls></audio>
+    </div>
     <div class="controller">
-      <button>P/S</button>
+      <button @click="pause">P/S</button>
     </div>
   </div>
 </template>
@@ -25,6 +28,43 @@ import ButtonReturn from '@/components/ButtonReturn/index.vue'
   },
 })
 export default class MiniPlayer extends Vue {
+  // NEED
+  // 暂停 finish
+  // 播放 finish
+  // 音量控制 finish
+  // 下载到本机
+  // 歌曲长度
+  // 当前播放到的时间 finish
+  // 改变播放时间 finish
+  private play () {
+    (this.$refs.audio as HTMLAudioElement).play()
+  }
+
+  private pause () {
+    (this.$refs.audio as HTMLAudioElement).pause()
+  }
+
+  private getVolume () {
+    const volume: number = (this.$refs.audio as HTMLAudioElement).volume
+    console.log(volume)
+  }
+
+  private setVolume (volume: number) {
+    (this.$refs.audio as HTMLAudioElement).volume = volume
+  }
+
+  private getCurrentTime () {
+    const currentTime = (this.$refs.audio as HTMLAudioElement).currentTime
+    console.log(currentTime)
+  }
+  private setCurrentTime (time: number) {
+    (this.$refs.audio as HTMLAudioElement).currentTime = time
+  }
+  
+  private getSongLength () {
+    const length: number = (this.$refs.audio as HTMLAudioElement).seekable.end(0)
+    console.log(length) 
+  }
 }
 </script>
 
